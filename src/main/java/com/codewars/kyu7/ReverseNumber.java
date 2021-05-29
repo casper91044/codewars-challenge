72eb857d138051b00368d8db81935c1486a7ad1d
casper91044 <casper91044@mail.ru>
2021-05-29 17:56:40 +0300

    Initial commit

---
+++ src/main/java/com/codewars/kyu7/ReverseNumber.java
@@ -0,0 +1,12 @@
+package main.java.com.codewars.kyu7;
+
+public class ReverseNumber {
+    public static int reverse(int number) {
+        int result = 0;
+        while(number != 0) {
+            result = result * 10 + number % 10;
+            number = number / 10;
+        }
+        return result;
+    }
+}
